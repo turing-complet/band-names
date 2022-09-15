8037e533256bb0b6763214848bb62adaf133837a
Jen Hagg <jhagg314@gmail.com>
2022-09-15 01:43:36 -0700

    move code out of notebook

--- band_names.py
+++ band_names.py
@@ -1,4 +1,6 @@
 from .hdf5_getters import get_artist_name, get_year, open_h5_file_read
+import json
+import pandas as pd
 
 
 def build_summary(root, limit=100):
@@ -20,5 +22,23 @@ def build_summary(root, limit=100):
 
 def convert(s):
     s = {k.decode(): v - {0} for k, v in s.items() if v != 0}
+    s = {k: v for k, v in s.items() if len(v) > 0}
     s = {k: [int(i) for i in v] for k, v in s.items()}
     return s
+
+
+def save(root):
+    summary = build_summary(root, limit=None)
+    s2 = convert(summary)
+    with open("summary.json", "w") as f:
+        json.dump(s2, f, indent=2)
+
+
+def main():
+    with open("summary.json") as f:
+        s2 = json.load(f)
+    s3 = {k: sorted(v)[0] for k, v in s2.items()}
+    df = pd.DataFrame({"artist": s3.keys(), "year": s3.values()})
+    df["label"] = df.artist.str.startswith("The")
+    df.label = df.label.apply(lambda b: "the" if b else "no_the")
+    df.hist(column="year", by="label")
